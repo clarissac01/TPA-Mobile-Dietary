d1caabdbd73d8bc09cd6509144e53af3efde7f79
Skolastika Gabriella <theresandia@gmail.com>
2021-05-17 03:26:02 +0700

    custom meal :)

---
+++ app/src/main/java/edu/bluejack20_2/dietary/AddCustomMealActivity.kt
@@ -0,0 +1,11 @@
+package edu.bluejack20_2.dietary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCustomMealActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_custom_meal)
+    }
+}
